737902f5bde3ee7204399983d808f75d32a8755e
cbouchy <cbouchy@hitpart.fr>
2019-07-23 12:34:31 +0200

    [Portfolio] Adding new button for see more projects

--- js/plugins.js
+++ js/plugins.js
@@ -1,12 +1,25 @@
-$(document).ready(function(){
-	//alert("toto");
+$(document).ready(function() {
 
-	/* FLECHE DE DEFILEMENT */
-	$("#top").click(function(event){
-		event.preventDefault(); //Désactivation des propriétés du a
-		$("html, body").animate({
-			scrollTop : 0
-		}, 900);
+    /* FLECHE DE DEFILEMENT */
+    $("#top").click(function(event) {
+        event.preventDefault(); //Désactivation des propriétés du a
+        $("html, body").animate({
+            scrollTop: 0
+        }, 900);
+    });
+
+    /* PORTFOLIO */
+    $("#see_more").click(function(event) {
+		$("#portfolio_more").show("slow");
+
+		$("#see_less").css("display", "inline-block");
+		$("#see_more").css("display", "none");
+    });
+
+    $("#see_less").click(function(event) {
+		$("#portfolio_more").hide("slow");
+
+		$("#see_more").css("display", "inline-block");
+		$("#see_less").css("display", "none");
 	});
-	
 });
